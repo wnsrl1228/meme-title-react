e1c379b120782d8b7381f9434192bc2f3c07e7a0
wnsrl <wnsrl981228@gmail.com>
2024-04-29 23:29:36 +0900

    환경변수 설정

--- src/constants/api.jsx
+++ src/constants/api.jsx
@@ -1,5 +1,5 @@
-export const KAKAO_AUTH_API_URL = "https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=5e126202c18a4006a3db7a30a84fadb4&redirect_uri=http://localhost:3000/login/kakao";
-export const BASE_URL = "http://localhost:8080";
+export const KAKAO_AUTH_API_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${process.env.REACT_APP_KAKAO_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}`;
+export const BASE_URL = `${process.env.REACT_APP_BASE_URL}`;
 
 export const HTTP_STATUS_CODE = {
     SUCCESS: 200,
@@ -12,8 +12,8 @@ export const HTTP_STATUS_CODE = {
     INTERNAL_SERVER_ERROR: 500,
   };
 
-  export const ERROR_CODE = {
-    EXPIRED_TOKEN: 4003,
-    INVALID_TOKEN: 4002,
-    INVALID_JWT_FORMAT: 5001
-  }
+export const ERROR_CODE = {
+  EXPIRED_TOKEN: 4003,
+  INVALID_TOKEN: 4002,
+  INVALID_JWT_FORMAT: 5001
+}
